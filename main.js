899e1c0bb5cf611156f39f2287a0a919bd0feb0d
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-05-24 11:26:26 -0700

    added config file

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, globalShortcut } = require("electron");
+const config = require("./config");
+
+let win;
 
 function createWindow() {
   // Create the browser window.
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
+    titleBarStyle: "hidden",
+    alwaysOnTop: true,
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
   // and load the index.html of the app.
-  win.loadFile("index.html");
+  win.loadURL(config.url);
+}
 
-  // Open the DevTools.
-  win.webContents.openDevTools();
+function toggleDevTools() {
+  win.webContents.toggleDevTools();
+}
+
+function createShortcuts() {
+  globalShortcut.register("CmdOrCtrl+J", toggleDevTools);
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+app.whenReady().then(createWindow).then(createShortcuts);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
